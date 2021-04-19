ef27c1b17e9633d58fea9a589217e171dd72ac31
The Noise <leespaulding25@gmail.com>
2021-04-19 18:49:39 -0400

    added activate handleer to clean up old Caches and return an array of name that are old

--- Develop/Public/service-worker.js
+++ Develop/Public/service-worker.js
@@ -3,4 +3,38 @@ const FILES_TO_CACHE = [
     '/',
     '/public/index.html',
     '/public/style.css',
-]
+];
+
+const CACHE_NAME ="static-cahce-v2";
+const RUNTIME_CACHE = "runtime-cache";
+
+self.addEventListener("install" , event => {
+    event.waitUnitl(
+        caches
+        .open(STATIC_CACHE)
+        .then(cache => cache.addAll(FILES_TO_CACHE))
+        .then(() => self.skipWaiting())
+    );
+});
+
+//cleaning up old caches
+self.addEventListener("activate" , event => {
+    const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
+    event.waitUntil(
+        caches
+            .keys()
+            .then( cacheNames => {
+                return cacheNames.filter(
+                    cacheName => !currentCaches.includes(cacheName)
+                );
+            })
+            .then(cacheToDelete => {
+                return Promise.all(
+                    cachesToDelete.map(cacheToDelete => {
+                        return caches.delete(cacheToDelete);
+                    })
+                );
+            })
+            .then(() => self.ClientRectList.claim())
+    );
+});
