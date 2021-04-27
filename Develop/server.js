5dcf3fe015bc9ec5f11646925c0011d7fef27479
The Noise <leespaulding25@gmail.com>
2021-04-26 22:51:46 -0400

    edited folder names

--- Develop/server.js
+++ Develop/server.js
@@ -3,8 +3,10 @@ const logger = require('morgan');
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const app = express();
+
 const PORT = process.env.PORT || 3000;
+
+const app = express();
 
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
