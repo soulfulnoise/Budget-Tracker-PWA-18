a5135ac3c6b2f91510a9535444c27447a707c69b
The Noise <leespaulding25@gmail.com>
2021-04-26 21:40:50 -0400

    added function to ensure sw are supportted and fixed qoute issue

--- Develop/Public/service-worker.js
+++ Develop/Public/service-worker.js
@@ -1,94 +1,78 @@
-//const { response } = require("express");
-
 //files to cache
 const FILES_TO_CACHE = [
-    '/',
-    '/public/index.html',
-    '/public/index.js',
-    '/mainfest.webmanifest',
-    '/public/style.css',
-    '/Public/Icons/icon-192x192.png',
-    '/Public.Icons/icon-512x512.png'
+    "/",
+    "/public/index.html",
+    "/public/index.js",
+    "/mainfest.webmanifest",
+    "/public/style.css",
+    "/Public/Icons/icon-192x192.png",
+    "/Public.Icons/icon-512x512.png"
 ];
 
 const CACHE_NAME ='static-cahce-v2';
 const DATA_CACHE_NAME = "data-cache-v1";
 
-self.addEventListener('install' ,  e => {
-    console.log('Service Worker: Install');
-    e.waitUnitl( async () => {
-         caches
-        .open(CACHE_NAME)
-        .then(cache => {
-            console.log('Service Worker: Caching Files');
-        cache.addAll(FILES_TO_CACHE)
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+// install
+self.addEventListener("install", function(evt) {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log("Your files were pre-cached successfully!");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+});
+//activate
+self.addEventListener("activate", function(evt) {
+  evt.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
         })
-        .then(() => self.skipWaiting())
-    }
-    );
+      );
+    })
+  );
+
+  self.clients.claim();
 });
 
-//cleaning up old caches
-self.addEventListener("activate" , event => {
-    const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
-    event.waitUntil(
-        caches
-            .keys()
-            .then( cacheNames => {
-                return cacheNames.filter(
-                    cacheName => !currentCaches.includes(cacheName)
-                );
-            })
-            .then(cacheToDelete => {
-                return Promise.all(
-                    cachesToDelete.map(cacheToDelete => {
-                        return caches.delete(cacheToDelete);
-                    })
-                );
-            })
-            .then(() => self.ClientRectList.claim())
-    );
-});
-
-self.addEventListener("fetch", (e) => {
-    // non GET requests are not cached and requests to other origins are not cached
-    if(
-        event.request.method !== "GET" ||
-        !event.request.url.startsWith(self.location.origin)
-    ) {
-        event.respondWith(fetch(event.request));
-        return;
-    }
-
-    //handle runtime GET requests for data from /api routes
-    if (event.request.url.includes("/api/transaction")) {
-  // make network request and fallback to cache if network request fails (offline)
-        event.respondWith(
-            caches.open(RUNTIME_CACHE).then(cache => {
-                return fetch(event.request)
-                    .then(response => {
-                        caches.put(event.request, response.clone());
-                        return response;
-                    })
-                    .catch(() => caches.match(event.request));
-            })
-        );
-        return;
-    }
-
-    event.respondWith(
-        caches.match(event.request).then(cachedResponse => {
-            if (cachesResponse) {
-                return cachedResponse;
+// fetch
+self.addEventListener("fetch", function(evt) {
+  // cache successful requests to the API
+  if (evt.request.url.includes("/api/")) {
+    evt.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(evt.request)
+          .then(response => {
+            //  clone and store  in the cache with a good response.
+            if (response.status === 200) {
+              cache.put(evt.request.url, response.clone());
             }
 
-            return caches.open(RUNTIME_CACHE).then(cache => {
-                return fetch(event.request).then(resposne => {
-                    return cache.put(event.request, response.clone()).then(() =>{
-                        return response;
-                    });
-                });
-            });
-        })
+            return response;
+          })
+          .catch(err => {
+            // Network request failed, try to get it from the cache.
+            return cache.match(evt.request);
+          });
+      }).catch(err => console.log(err))
     );
+
+    return;
+  }
+
+  // if the request is not for the API, serve static assets using "offline-first" approach.
+  evt.respondWith(
+    caches.match(evt.request).then(function(response) {
+      return response || fetch(evt.request);
+    })
+  );
 });
