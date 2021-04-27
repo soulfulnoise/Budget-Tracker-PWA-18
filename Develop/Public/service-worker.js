68dfd52aa18d3643393c0101c4ffc22dd90c257a
The Noise <leespaulding25@gmail.com>
2021-04-26 21:51:22 -0400

    trying something

--- Develop/Public/service-worker.js
+++ Develop/Public/service-worker.js
@@ -12,8 +12,6 @@ const FILES_TO_CACHE = [
 const CACHE_NAME ="static-cahce-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
 
 // install
 self.addEventListener("install", function(evt) {
